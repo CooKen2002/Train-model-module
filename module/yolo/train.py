e3f8ffa86c9740926c8199d88d7b8678cd58ae98
CooKen2002 <ndcuong.cooken@gmail.com>
2026-07-18 09:01:41 +0700

    tesing with other model

--- module/yolo/train.py
+++ module/yolo/train.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8n-pose.pt")
-results = model.train(data="data.yaml", epochs=100, imgsz=640, batch=32)
+model = YOLO("yolo11n-pose.pt")
+
+model.train(data="data.yaml", epochs=100, imgsz=640, batch=16)
